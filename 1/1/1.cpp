ff9bf21b1071ad5fc4643f0621d0200feed4bb2e
Igor Kastyrkin <kastyrkinIV@ya.ru>
2023-10-27 19:56:08 +0300

    some changes

--- 1/1/1.cpp
+++ 1/1/1.cpp
@@ -5,67 +5,37 @@
 #include "SqlSelectQueryBuilder.h"
 
 
-struct Event {
-    std::string name;
-    std::string message;
-    std::string description;
-    int level;
-    std::string action;
-    bool isActive;
-    std::chrono::time_point<std::chrono::steady_clock> timestamp;
-    uint8_t ttl;
-    std::string target;
-    std::string source;
-    std::vector<std::string> route;
-    std::map<std::string, std::string> meta;
-};
+void Send(const Query& q)
+{
+    std::string out = "SELECT ";
+    for (const auto& a : q.colomn)
+    {
+        out += a;
+        out += ", ";
+    }
+    out += "\b\b";
+    out += " FROM " + q.table + " WHERE ";
+    for (const auto& b : q.mp)
+    {
+        out += b.first + " = " + b.second;
+        out += " AND ";
+    }
+    out += "\b\b\b\b\b";
+    out += ";      ";
 
+    std::cout <<out<< std::endl;
 
-class EventBuilder {
-public:
-    explicit EventBuilder(const std::string& event_name) {
-        event.name = event_name;
-        event.description = "This is " + event_name;
-        event.timestamp = std::chrono::steady_clock::now();
-    }
-
-    Event BuildEvent() noexcept {
-        if (event.source.empty() && !event.route.empty()) {
-            event.source = event.route.front();
-        }
-        if (event.target.empty() && !event.route.empty()) {
-            event.target = event.route.back();
-        }
-        return event;
-    }
-    EventBuilder& AddLevel(int level) noexcept {
-        event.level = level;
-        return *this;
-    }
-
-    EventBuilder& AddMeta(const std::string& key, const std::string& value) {
-        event.meta[key] = value;
-        return *this;
-    }
-    EventBuilder& AddRoutePoint(const std::string& point) {
-        event.route.push_back(point);
-        return *this;
-    }
-private:
-    Event event;
-};
-
-void SendEvent(const Event& e) {
-    std::cout << "Sending event " << e.name << " to " << e.target << " from " << e.source << std::endl;
-    std::cout << "route is: \n";
-    for (const auto& point : e.route) {
-        std::cout << point << "->";
-    }
 }
 
-int main() {
-    EventBuilder builder("VeryImportantEvent");
-    builder.AddLevel(3).AddMeta("key", "value");
-    builder.AddRoutePoint("first").AddRoutePoint("second");
-    SendEvent(builder.BuildEvent());
+int main()
+{
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColomn("name").AddColomn("phone");
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+
+    Send(query_builder.BildSql());
+
+    return 0;
+
 }
--- 1/1/SqlSelectQueryBuilder.h
+++ 1/1/SqlSelectQueryBuilder.h
@@ -5,7 +5,7 @@
 
 struct Query {
 	std::vector<std::string> colomn;
-	std::vector<std::string> from;
+	std::string table;
 	std::map<std::string, std::string> mp;
 };
 
@@ -17,12 +17,6 @@ public:
 
 	};
 
-	/*
-	in this method, we select the columns that we will output 
-	based on the result of the SQL query. Let's say we have 10 columns
-	in the table for information about students
-	(full name, average grade, place of residence, course, etc.),
-	but now it is important for us to display only the full name and phone number.*/
 
 	SqlSelectQueryBuilder& AddColomn(std::string st)
 	{
@@ -32,24 +26,20 @@ public:
 	//this is the function we need to specify the tables we are accessing.
 	SqlSelectQueryBuilder& AddFrom(std::string st)
 	{
-		query.from.push_back(st);
+		query.table = st;
 		return *this;
 	}
-	/*
-	this is already generating a condition. And what kind of students do we need? In the example, this is a student with the name John and the ID 42.
-	query_builder.AddWhere(“id”, “42”).AddWhere(“name”, “John”);
-	Taking into account that the id is most likely unique, then we will have one person. We can only leave:
-	query_builder.AddWhere(“name”, “John”);
-	Then a list of all students with the name John will be displayed.*/
-	SqlSelectQueryBuilder& AddWhere(std::pair<std::string, std::string> vp)
+
+	SqlSelectQueryBuilder& AddWhere(std::string a, std::string b)
 	{
-		query.mp.emplace(vp);
+		query.mp.emplace(std::pair<std::string, std::string>(a,b));
 		return *this;
 	}
 
 	// should return final query for SQL database
 	Query BildSql()
 	{
+		if (query.colomn.empty()) query.colomn.push_back("*");
 		return query;
 	}
 
